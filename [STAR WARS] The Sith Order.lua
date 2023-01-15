8044b9aca1c7863dd50439c97e2e573555a88694
SigurdOrUsername <97767396+SigurdOrUsername@users.noreply.github.com>
2023-01-15 23:07:12 +0100

    Create [STAR WARS] The Sith Order.lua

---
+++ [STAR WARS] The Sith Order.lua
@@ -0,0 +1,40 @@
+local Players = game:GetService("Players")
+local Player = Players.LocalPlayer
+
+local function GetEnemies()
+    local Enemies = {}
+
+    for Index, Enemy in next, Players:GetPlayers() do
+        if Enemy ~= Player and Enemy.Character and not Enemy.Character:FindFirstChild("ForceField") and Enemy.Character:FindFirstChild("Humanoid") then
+            table.insert(Enemies, Enemy)
+        end
+    end
+
+    return Enemies
+end
+
+while task.wait() do
+    local Tool = Player.Character:FindFirstChildWhichIsA("Tool")
+    
+    if Tool then
+        local Enemies = GetEnemies()
+        warn(#Enemies)
+        if #Enemies > 0 then
+            for Index, Enemy in next, Enemies do
+                while Enemy.Character and Enemy.Character:FindFirstChild("Humanoid") and Enemy.Character.Humanoid.Health > 0 do
+                    Player.Character.HumanoidRootPart.CFrame = Enemy.Character.HumanoidRootPart.CFrame * CFrame.new(0, -10, 0)
+                    Tool.Core.Comm:FireServer({
+                        1,
+                        Enemy.Character.Humanoid,
+                        10,
+                        14.8,
+                        Vector3.new()
+                    })
+                    task.wait()
+                end
+            end
+        else
+            Player.Character.HumanoidRootPart.CFrame = CFrame.new(0, 5000000, 0)
+        end
+    end
+end
